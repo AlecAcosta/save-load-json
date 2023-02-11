cecf94223512bc8114e5851f838ca3d689a3c67b
AlecAcosta <64831347+AlecAcosta@users.noreply.github.com>
2023-02-10 20:47:54 -0600

    added buttons and save functionality

--- Main.gd
+++ Main.gd
@@ -1,23 +1,33 @@
 extends Node
 
+onready var label = $Label
+
+var data: Dictionary = {"attack": 50, "defense": 50, "life": 50}
+var savefile:= "user://data.json"
+
 
 func _ready():
-	var data = loadJson("res://myFile.json")
-	
-	#print the entire Dictionary (var data)
-	$Label.text = String(data)
-
-	#print the first item in "items"
-	$Label.text += "\n\n" + String(data["items"][0])
+	label.text = "default data: " + str(data)
 
 
+func _on_Save_pressed():
+	var file = File.new()
+	file.open(savefile, File.WRITE)
+	file.store_line(to_json(data))
+	file.close()
+	label.text = "saved data"
 
-func loadJson(_jsonDir):
-	_jsonDir
-	var jsonFile = File.new()
-	var jsonData: Dictionary
-	jsonFile.open(_jsonDir,File.READ)
-	jsonData = parse_json(jsonFile.get_as_text())
-	jsonFile.close()
-	
-	return jsonData
+func _on_Load_pressed():
+	var file = File.new()
+	if file.file_exists(savefile):
+		file.open(savefile, File.READ)
+		data = parse_json(file.get_as_text())
+		file.close()
+		label.text = "loaded data: " + str(data)
+	else:
+		label.text = "there's no saved data"
+
+func _on_Delete_pressed():
+	var dir = Directory.new()
+	if dir.file_exists(savefile):
+		dir.remove(savefile)
